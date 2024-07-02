8bc0199dfd884709e095d1b36ade381595879d07
Rachel Katz <rachel.katz@flatironschool.com>
2024-07-02 11:09:46 -0400

    demo done

--- src/index.js
+++ src/index.js
@@ -1,64 +1,64 @@
-/*
+//get the dog-bar element
+//get the dog-info element
+const info = document.getElementById("dog-info");
+const nav = document.getElementById("dog-bar");
+const url = "http://localhost:3000";
 
-REMEMBER TO PUT DEFER IN INDEX.HTML'S SCRIPT TAG
-
-*/
-//global constants
-const dogBar = document.querySelector('#dog-bar');
-const dogInfo = document.querySelector('#dog-info');
-const url = 'http://localhost:3000/pups';
-
-//add dog to span bar
-const addToBar = (dog) => {
-    let span = document.createElement('span');
-    span.innerText = dog.name;
-
-    //on span click pass dog to showDogInfo
-    span.addEventListener('click', (e) => showDogInfo(dog))
-
-    dogBar.append(span);
+function renderDog(dog) {
+	//clear out the info div of all previous dogs
+	//before populating with new dog
+	info.innerHTML = "";
+	const img = document.createElement("img");
+	const h2 = document.createElement("h2");
+	const button = document.createElement("button");
+	img.src = dog.image;
+	h2.textContent = dog.name;
+	if (dog.isGoodDog) {
+		button.textContent = "Good Dog!";
+	} else {
+		button.textContent = "Bad Dog!";
+	}
+	//add another event listener to button
+	button.addEventListener("click", (e) => {
+		//check button content (if/else)
+		//e.target.textContent, button.textContent
+		let curVal = e.target.textContent;
+		if (curVal === "Good Dog!") {
+			e.target.textContent = "Bad Dog!";
+		} else {
+			e.target.textContent = "Good Dog!";
+		}
+	});
+	info.append(img);
+	info.append(h2);
+	info.append(button);
 }
 
-//show info for one dog
-const showDogInfo = (dog) => {
-    //clear dogInfo div from any previous dogs
-    dogInfo.innerHTML = '';
-
-    //create elements 
-    let dogImg = document.createElement('img');
-    let dogName = document.createElement('h2');
-    let dogBtn = document.createElement('button');
-
-    //put info in elements
-    dogImg.src = dog.image;
-    dogName.innerText = dog.name;
-    //use terneray on isGoodDog value to test wheter button should say 'Good Dog!' or 'Bad Dog!'
-    dogBtn.innerText = dog.isGoodDog ? 'Good Dog!' : 'Bad Dog!';
-
-    //on dogBtn click toggle 'good dog' and 'bad dog' (does not persist)
-    dogBtn.addEventListener('click', (e) => toggleBtn(e.target)); //e.target will be dogBtn
-
-    //append everything to dogInfo
-    dogInfo.append(dogName, dogImg, dogBtn);
-}
-
-//toggle button text between 'Good Dog!' and 'Bad Dog!' 
-const toggleBtn = function(btn) {
-    //get current text
-    let curText = btn.innerText;
-    //update current text
-    if(curText === 'Good Dog!') {
-        btn.innerText = 'Bad Dog!'
-    } else {
-        btn.innerText = 'Good Dog!'
-    }
-}
-
-//fetch all dogs
-fetch(url)
-.then(res => res.json())
-.then(data => {
-    data.forEach((dog, i) => {
-        addToBar(dog)
-    })
-})
+//make fetch statement to grab the info
+fetch(`${url}/pups`) //returns a promise
+	.then((res) => {
+		if (res.ok) {
+			return res.json();
+		} else {
+			throw "no";
+		}
+	})
+	.then((data) => {
+		//create new elements: img, h2, button
+		//update those elements values
+		//append to info
+		//use data we already have (via our fetch) and look at first dog
+		renderDog(data[0]);
+		//foreach dog create a new nav/span element
+		data.forEach((dog) => {
+			//create a new element <span> and put in dog name
+			let span = document.createElement("span");
+			span.textContent = dog.name;
+			//append span to dog-bar
+			nav.append(span);
+			//addEventListener for click (span)
+			span.addEventListener("click", () => {
+				renderDog(dog);
+			});
+		});
+	});
